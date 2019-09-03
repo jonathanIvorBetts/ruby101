d2181b1e7d62bf127d1cef48655e70c4ee998af1
jonathanIvorBetts <jonathan.betts@uwe.ac.uk>
2019-09-03 15:06:30 +0100

    Second commit. Test from Rubymine source ctrl
    Signed-off-by: jonathanIvorBetts <jonathan.betts@uwe.ac.uk>

---
+++ 4_standard_types/arrays.rb
@@ -0,0 +1,6 @@
+# [1,2,3]
+
+# init like
+arr = []
+# new new new
+arr1 = Arra
--- 4_standard_types/regexp.rb
+++ 4_standard_types/regexp.rb
@@ -9,7 +9,7 @@ reg = %r(\d+/\d+)
 # =~ match
 # !~ not match
 
-puts /\d+/ =~ "hey, no idea what d slash plus it's 12:00 midday" 
+puts /\d+/ =~ "hey, no idea what d slash plus it's 12:00 midday"
 # ok returned first index of time in the string
 
 m = /(\d+):(\d+)/.match("in this string is a colon, time sep e.g. 14:56, you gets me?")
