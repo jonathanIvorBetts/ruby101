3c349863d01668d3c7cd14bd01a7188e977c4157
jonathanIvorBetts <jonathan.betts@uwe.ac.uk>
2019-09-04 10:18:58 +0100

    no message

--- 4_standard_types/arrays.rb
+++ 4_standard_types/arrays.rb
@@ -3,4 +3,32 @@
 # init like
 arr = []
 # new new new
-arr1 = Arra
+arr1 = Array.new(5)
+
+arr2 = Array.new(2, true)
+
+arr3 = Array.new(2, "abc")
+arr3.first.upcase!
+puts arr3
+
+arr4 = Array.new(25) {"abc"}
+pos = arr4.length-6
+
+arr4[pos].upcase!
+puts arr4
+
+puts arr4.length
+
+# array of symbols instantiated with %i
+arr_sym = %i(w a s d)
+puts arr_sym
+
+puts arr_sym.size
+
+
+# slicing up eyballs
+arr = [1,2,3,4,5,6,7,8,9]
+puts arr[2..6]
+
+# math rock stuff
+[12345, 2,3,4,5] - [3, 5]
--- 4_standard_types/enum.rb
+++ 4_standard_types/enum.rb
@@ -1,1 +1,16 @@
 
+# map
+[5, 10, 15, 20, 25, 30, "allthewhile"].map { |v| v * 6}
+# reduce
+arr = [1,3,5,7,2,4,6,8]
+# sort
+arr = [1,3,5,7,2,4,6,8].reverse
+# select
+arr = [1,3,5,7,2,4,6,8]
+arr.select{ |x| x.even? }
+puts arr
+arr.select!{ |x| x.even? }
+puts arr
+
+# each cons for sequential iterative operations e.g.
+[1,2,3,4,5].each_cons(3) { |v| p v }
