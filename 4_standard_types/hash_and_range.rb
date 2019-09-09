65fc396214f08d4384b81572b53cd57022dc6db3
jonathanIvorBetts <jonathan.betts@uwe.ac.uk>
2019-09-09 09:53:07 +0100

    Second commit. Test from Rubymine source ctrl
    Signed-off-by: jonathanIvorBetts <jonathan.betts@uwe.ac.uk>

---
+++ 4_standard_types/hash_and_range.rb
@@ -0,0 +1,25 @@
+
+# KVP e.g.
+#
+h = {}
+h = {"min" => 0,  "max" => 100}
+h = {min: 0, max: 100}
+
+# create
+h = {a: "a", b: "b"}
+# access
+h[b]
+# update
+h[b] = "z"
+
+# define a default
+h = Hash.new { |, | [] =  }
+
+# iterate using each
+#
+
+
+
+# ranges
+#  (1..99)
+#  begin, end, include
--- 4_standard_types/parallel_assignment.rb
+++ 4_standard_types/parallel_assignment.rb
@@ -1,1 +1,25 @@
 
+# parallel ass
+a =1,2,3,4,5
+puts a
+a,b = 1,2,3,4
+puts a
+puts b
+
+def get_values
+  [1,2,3,4]
+end
+
+a, b = get_values
+puts a, b
+
+#
+#
+# splat * in variable assignment cn be on left or right
+# it's greedy
+#
+first, _, _, _, last = 1, 2, *[7,8,9]
+puts first
+puts last
+puts _
+puts _,_
